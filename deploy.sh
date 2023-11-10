5f0feb9f4d0e72aef0acc5e38d8f32f2462f6a7f
Amir Ehsandar <ehsandaramir@gmail.com>
2023-11-10 16:17:36 +0330

    New theme and deployment process

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 
-rsync -avz . htz:/root/ehsandar.com
-ssh htz bash -c 'cd /root/ehsandar.com && /root/go/bin/hugo'
-ssh htz bash -c '/root/go/bin/caddy reload --config /root/ehsandar.com/Caddyfile'
+rsync -avz public/ htz:/var/www/ehsandar.top/public/
